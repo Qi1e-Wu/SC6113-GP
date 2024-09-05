04566f2cab26c7df4191804cbc8e0eed6cb8f0bd
Shao Zixuan <117291172+szxshawn@users.noreply.github.com>
2024-09-05 12:39:49 +0000

    1

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask,render_template,request
-
+import sqlite3
+import datetime
 app=Flask(__name__)
 
 @app.route('/',methods=["get","post"])
 def index():
     return render_template('index.html')
+
+@app.route('/main',methods=["get","post"])
+def main():
+    r = request.form.get("q")
+    current_time = datetime.datetime.now()
+    conn = sqlite3.connect('/content/drive/MyDrive/Colab Notebooks/Practice/2024 SC6113 T1/dapp.db')
+    c = conn.cursor()
+    c.execute("insert into user values (?, ?)", (r, current_time))
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('main.html', r=r)
 
 @app.route('/store_money',methods=["get","post"])
 def store_money():
